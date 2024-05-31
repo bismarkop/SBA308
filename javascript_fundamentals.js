900c6461976128d8164686c3fd906b06291c26c3
biso <bopoku89@gmail.com>
2024-05-31 16:48:23 -0400

    updates

--- javascript_fundamentals.js
+++ javascript_fundamentals.js
@@ -84,14 +84,30 @@ const LearnerSubmissions = [
 
 // Get the ID of the learner for which this data has been collected
 
-function getLearnerID(submissions, id) {
+function getLearnerID(submissions) {
+    let student_id = []
+    let unique_id = []
     for (let i = 0; i < submissions.length; i++) {
-        if (submissions[i].learner_id === id) {
-            return id
-        }
+        if (typeof(submissions[i].learner_id) === 'number') {
+            student_id.push(submissions[i].learner_id)
+        
     }
+    console.log(student_id)
+    
+    // function removeDuplicates(arr) {
+    //     let student_id = []
+    //     arr.forEach(element => {
+    //         if(!student_id.includes(element)) {
+    //             student_id.push(element)
+    //         }
+    //     })
+    //     return student_id
+    // }
+
+
 }
-getLearnerID(LearnerSubmissions, 132)
+
+console.log(getLearnerID(LearnerSubmissions))
 
 
 // Get the learner’s total, weighted average, in which assignments with more points_possible should be counted for more e.g. a learner with 50/100 on one assignment and 190/200 on another would have a weighted average score of 240/300 = 80%. Each assignment should have a key with its ID and the value associated with it should be the percentage that the learner scored on the assignment (submission.score / points_possible)
@@ -122,6 +138,7 @@ function getWeightedAvg(submissions, assignmentGroup) {
                 totalWeightedScore += sub.submission.score
             }
         })
+        // Turns weighted average into a percentage
         let weightedAvg = (totalWeightedScore / totalPointsPossible) * 100
         results[learnerID] = weightedAvg
     }
@@ -129,9 +146,28 @@ function getWeightedAvg(submissions, assignmentGroup) {
 }
 
 let weightedAvgs = getWeightedAvg(LearnerSubmissions, AssignmentGroup.assignments)
-console.log(weightedAvgs)
+// console.log(weightedAvgs)
 
-// Total weigted average = actual score totals / total points possible
+function assignmentScores(submissions, scores) {
+    let keys = Object.keys(submissions)
+    let values = Object.values(submissions)
+    let collectData = []
+
+    // Cycle through LearnerSubmissions for the learner_id
+    for (let i = 0; i < submissions.length; i++) {
+        if (typeof(submissions[i].learner_id) === "number") {
+            collectData.push(submissions[i].learner_id, submissions[i].submission.score)
+            console.log(`${submissions[i].learner_id}: ${submissions[i].submission.score}`)
+            
+            // collectData[keys[i]] = values[i]
+        }
+    } 
+    // console.log(keys)
+    // console.log(values)
+    console.log(collectData)
+}
+// assignmentScores(LearnerSubmissions)
+
 
 // Each assignment should have a key with its ID (1, 2, or 3) and the value of the learner's score on that particular assignment = 1: .90, 2: .87
 
@@ -146,10 +182,14 @@ console.log(weightedAvgs)
 
 
 
-// If an AssignmentGroup does not belong to its course (mismatching course_id), your program should throw an error, letting the user know that the input was invalid. Similar data validation should occur elsewhere within the program.
 
 
 //            ***** Error Check Functions *****
+// If an AssignmentGroup does not belong to its course (mismatching course_id), your program should throw an error, letting the user know that the input was invalid. Similar data validation should occur elsewhere within the program.
+
+//You should also account for potential errors in the data that your program receives. What if points_possible is 0? You cannot divide by zero. What if a value that you are expecting to be a number is instead a string? Use try/catch and other logic to handle these types of errors gracefully.
+
+
 function matchIds(course, assignment) {
     try {
         if (course.id === assignment.course_id) {
@@ -163,17 +203,15 @@ function matchIds(course, assignment) {
 }
 // matchIds(CourseInfo, AssignmentGroup)
 
-function checkPoints() {
 
-}
-//            ***** Error Check Functions End *****
+//            ***** Error Check Functions Section End *****
 
 /* Create a function named getLearnerData() that accepts these values as parameters, in the order listed: (CourseInfo, AssignmentGroup, [LearnerSubmission]), and returns the formatted result, which should be an array of objects as described above. You may use as many helper functions as you see fit.*/
 
-function getLearnerData(courseInfo, assignmentGroup, [learnerSubmission]) {
-    return {
-        id: id,
-        avg: number,
-        assignment_id: number
-    }
-}
+// function getLearnerData(courseInfo, assignmentGroup, [learnerSubmission]) {
+//     return {
+//         id: id,
+//         avg: number,
+//         assignment_id: number
+//     }
+// }
