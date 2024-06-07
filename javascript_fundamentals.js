28e84222d5029e989c5ca1de9e607c2340c8c073
biso <bopoku89@gmail.com>
2024-06-07 00:06:44 -0400

    Updates and notes

--- javascript_fundamentals.js
+++ javascript_fundamentals.js
@@ -84,10 +84,19 @@ const LearnerSubmissions = [
 
 // Get the learner’s total, weighted average, in which assignments with more points_possible should be counted for more e.g. a learner with 50/100 on one assignment and 190/200 on another would have a weighted average score of 240/300 = 80%. Each assignment should have a key with its ID and the value associated with it should be the percentage that the learner scored on the assignment (submission.score / points_possible)
 
+// Each assignment should have a key with its ID (1, 2, or 3) and the value of the learner's score on that particular assignment = 1: .90, 2: .87
+
+// if an assignment is not yet due, it should not be included in either
+// the average or the keyed dictionary of scores
+// } ---?? Come back to this
+
+/* Create a function named getLearnerData() that accepts these values as parameters, in the order listed: (CourseInfo, AssignmentGroup, [LearnerSubmission]), and returns the formatted result, which should be an array of objects as described above. You may use as many helper functions as you see fit.*/
+
+
 
 function getLearnerData (courseInfo, assignmentGroup, learnerSubmissions) {
     matchIds(CourseInfo, AssignmentGroup)
-    
+    // Make points possible more easily accessible
     const findPointsPossible = assignmentGroup.assignments.reduce ((map, assignment) => {
         map[assignment.id] = assignment.points_possible
         return map
@@ -96,6 +105,7 @@ function getLearnerData (courseInfo, assignmentGroup, learnerSubmissions) {
     let finalGrades = {}
 
     learnerSubmissions.forEach(submission => {
+        //Destructured code to get the information to show similar to the example
         const {learner_id, assignment_id, submission: {score}} = submission
         const pointsPossible = findPointsPossible[assignment_id]
         const percentageScore = (score / pointsPossible) * 100
@@ -115,20 +125,7 @@ console.log(finalGrades)
 
 
 
-// Each assignment should have a key with its ID (1, 2, or 3) and the value of the learner's score on that particular assignment = 1: .90, 2: .87
-
-
-
-// getLearnerAvg(LearnerSubmissions, getLearnerID(LearnerSubmissions, 132), AssignmentGroup)
-
-//     <assignment_id>: number,
-// if an assignment is not yet due, it should not be included in either
-// the average or the keyed dictionary of scores
-// }
-
-
-
-//            ***** Error Check Functions *****
+//            ***** Error Check Function *****
 // If an AssignmentGroup does not belong to its course (mismatching course_id), your program should throw an error, letting the user know that the input was invalid. Similar data validation should occur elsewhere within the program.
 
 //You should also account for potential errors in the data that your program receives. What if points_possible is 0? You cannot divide by zero. What if a value that you are expecting to be a number is instead a string? Use try/catch and other logic to handle these types of errors gracefully.
@@ -150,4 +147,4 @@ function matchIds(course, assignment) {
 
 //            ***** Error Check Functions Section End *****
 
-/* Create a function named getLearnerData() that accepts these values as parameters, in the order listed: (CourseInfo, AssignmentGroup, [LearnerSubmission]), and returns the formatted result, which should be an array of objects as described above. You may use as many helper functions as you see fit.*/
+
